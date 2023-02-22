3b910da3b844fc7e3842f75c8cb378c2a9f9516a
ktdykstra <ktdykstra@gmail.com>
2023-02-22 17:05:28 +0800

    edits

--- app.py
+++ app.py
@@ -1,14 +1,33 @@
 from flask import Flask, render_template
 from flask import jsonify
 import socket 
+import pickle
 
 app = Flask(__name__)
 
 # two decorators, same function
 @app.route('/')
-@app.route('/index.html')
-def index():
-    return render_template('index.html', the_title='Home Page')
+@app.route('/home.html')
+def home():
+    return render_template('home.html', the_title='Home Page')
+
+@app.route("/predict", methods=['GET','POST'])
+def predict():
+    return render_template("home.html")
+    if request.method == 'POST':
+        #access the data from form
+        ## Age
+        age = int(request.form["age"])
+        bmi = int(request.form["bmi"])
+        children = int(request.form["children"])
+        Sex = int(request.form["Sex"])
+        Smoker = int(request.form["Smoker"])
+        Region = int(request.form["Region"])
+        #get prediction
+        input_cols = [[age, bmi, children, Sex, Smoker, Region]]
+        prediction = model.predict(input_cols)
+        output = round(prediction[0], 2)
+        return render_template("home.html", prediction_text='Your predicted annual Healthcare Expense is $ {}'.format(output))
 
 @app.route('/ip')
 def ip():
