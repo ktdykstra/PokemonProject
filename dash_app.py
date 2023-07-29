8e3bbc3d5c210c027fe59b437bcda73947d66d90
nkeeley <57279782+nkeeley@users.noreply.github.com>
2023-07-28 23:14:58 -0400

    updated plots, cleaned stats, and added section headers
    the plots are now sorted, have axes, and there are both hero and villain plots

--- dash_app.py
+++ dash_app.py
@@ -6,4 +6,22 @@ Created on Fri Jul 28 14:33:40 2023
 @author: nkeeley
 """
 
-print("hello")
+# # test function
+# def test_this():
+    
+#     print("hello")
+#     return
+
+from dash import Dash, html
+
+def create_dash(flask_app):
+    
+    dash_instance=Dash(server=flask_app,name="Dashboard",url_base_pathname="/dash/")
+    dash_instance.layout = html.Div([
+        html.Div(children='Hello World')
+    ])
+
+    return dash_instance
+
+
+
